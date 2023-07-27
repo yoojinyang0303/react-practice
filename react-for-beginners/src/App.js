ba048a97939ad4e3fef0e35d4041409f99426bfd
[SW5] 양유진 <yoojinyang0303@gmail.com>
2023-07-27 14:49:09 +0900

    feat: Chap06 - EFFECTS - useEffect, Deps

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,10 +1,41 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  console.log("I run all the time");
+
+  // 이렇게 한 번만 호출할 함수는 뭐가 있을까?  -- 예) API 호출
+  useEffect(() => {
+    console.log("I run only once!");
+  }, []);
+
+  useEffect(() => {
+    // console.log("Search for", keyword);
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text="Button" />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>Welcome, I'm a Counter</h1>
+      <p style={{ color: "blue" }}>Counting: {counter}</p>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
