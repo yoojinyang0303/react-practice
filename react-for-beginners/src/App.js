93be36a6c5bab56edd2550a07e189879aaca8849
[SW5] 양유진 <yoojinyang0303@gmail.com>
2023-07-28 15:46:44 +0900

    feat: Chap07 - PRACTICE MOVIE APP - Coin Tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,39 +1,26 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (e) => setToDo(e.target.value);
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-
-    // Add To Do 클릭 시, "함수" 이용하여 투두리스트에 toDo 담기
-    setToDos((currArray) => [toDo, ...currArray]);
-
-    // Add To Do 후, input에 있는 toDo 값 reset
-    setToDo("");
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  // API 호출
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Do List</h1>
-      <h3>to Dos: {toDos.length}</h3>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
       <ul>
-        {toDos.map((toDo, index) => (
-          <li key={index}>{toDo}</li>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
+          </li>
         ))}
       </ul>
     </div>
